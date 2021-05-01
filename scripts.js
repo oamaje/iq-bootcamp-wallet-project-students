c8ec97a7430d2d0078cbc66e1d59eb749b1dd49a
Avraz Zebary <savraz123@gmail.com>
2021-05-01 18:22:39 +0300

    fix close buttons

--- scripts.js
+++ scripts.js
@@ -1,26 +1,38 @@
 console.log("hello Re:codedededededededed");
 //----- modal --------//
-let modal = document.getElementById("myModal");
+
 
 // Get the button that opens the modal
-let btn = document.getElementById("ctrwallet");
+
 
 // Get the <span> element that closes the modal
-let span = document.getElementById("closebtn")[0];
+// let span = document.getElementById("closebtn")[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+// btn.onclick = function() {
+//   modal.style.display = "block";
+// }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+
+const mainPage = document.getElementById("mainPage");
+const createbtn = document.getElementById("ctrwallet");
+const modal = document.getElementById("mymodal");
+const closeButtons = document.getElementsByClassName("closeButtons");
+
+
+createbtn.addEventListener("click", function(){
+    mainPage.style.display = "none";
+    modal.style.display  = "block";
+});
+
+
+closeButtons.addEventListener("click", function(){
+  modal.style.display = "none";
+  mainPage.style.display = "block";
+});
