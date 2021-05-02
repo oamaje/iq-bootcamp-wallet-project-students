cdb6f38a04f5c07807870f057ce9fc7959188ec9
omar-bassam-ammaj <oamaje@gmail.com>
2021-05-02 12:21:30 +0300

    finished little bit of js and modified the css for the 2nd page

--- scripts.js
+++ scripts.js
@@ -6,44 +6,66 @@ const modal = document.getElementById("mymodal");
 const closeButtons = document.getElementById("closeButtons");
 const closeButtonss = document.getElementById("closeButtonss");
 
-
 const walletPage = document.getElementById("wallet-page");
 const createWallet2 = document.getElementById("btnctrwallet");
 
-
-createbtn.addEventListener("click", function(){
-    mainPage.style.display = "none";
-    modal.style.display  = "block";
+createbtn.addEventListener("click", function () {
+  mainPage.style.display = "none";
+  modal.style.display = "block";
 });
 
-
-closeButtons.addEventListener("click", function(){
+closeButtons.addEventListener("click", function () {
   // alert("testing");
   modal.style.display = "none";
-  mainPage.style.display = "block";  
+  mainPage.style.display = "block";
 });
 
-closeButtonss.addEventListener("click", function(){
+closeButtonss.addEventListener("click", function () {
   // alert("testing");
   modal.style.display = "none";
-  mainPage.style.display = "block";  
+  mainPage.style.display = "block";
 });
 
-
-createWallet2.addEventListener("click", function(){
+createWallet2.addEventListener("click", function () {
   modal.style.display = "none";
-  walletPage.style.display = "block"; 
+  walletPage.style.display = "flex";
+
+  const unameval = document.getElementById("uname");
+  const walletpagename = document.getElementById("walletpagename");
+  const umoneyval = document.getElementById("umoney");
+
+  function getcurrency() {
+    if (document.getElementById("usdd").checked) {
+      return "$";
+    } else if (document.getElementById("iraqiDinar").checked) {
+      return "IQD";
+    }
+  }
+  walletpagename.innerHTML = `${unameval.value} Wallet Balance: ${umoneyval.value} ${getcurrency()}`;
+});
+ const addtrans = document.getElementById("addTransaction").addEventListener("click",
+function walletpg() {
+  const transaction = document.getElementById("makeTransaction");
+  const plus = document.getElementById("incomeButton");
+  const minus = document.getElementById("expenseButton");
+  const notes = document.getElementById("transactionNote");
+  const tags  = document.getElementById("transactionTag");
+  
+
 })
 
 
 
+
+
+
+
+makeTransaction
 class Wallet {
-  constructor(name, description, balance, currency){
+  constructor(name, description, balance, currency) {
     this.name = name;
     this.description = description;
     this.balance = balance;
     this.currency = currency;
   }
-
-  
 }
